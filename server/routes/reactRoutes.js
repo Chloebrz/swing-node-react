78c36d138b87c8ec211c315713741e9589d9e36c
Chloebrz <chloe.brouzes@hotmail.fr>
2017-08-30 17:52:29 +0200

    Non logged in users trying to access /admin/* page redirected to home page

--- server/routes/reactRoutes.js
+++ server/routes/reactRoutes.js
@@ -2,6 +2,30 @@
 const path = require("path");
 
 module.exports = app => {
+    /**
+     * GET /admin
+     * Check if the user trying to access the /admin page is logged in. If not, redirect to home page.
+     */
+    app.get("/admin", (req, res) => {
+        if (req.user) return res.sendFile(path.resolve(__dirname, "../..", "public", "index.html"));
+
+        res.redirect("/");
+    });
+
+    /**
+     * GET /admin/*
+     * Check if the user trying to access the any admin page is logged in. If not, redirect to home page.
+     */
+    app.get("/admin/*", (req, res) => {
+        if (req.user) return res.sendFile(path.resolve(__dirname, "../..", "public", "index.html"));
+
+        res.redirect("/");
+    });
+
+    /**
+     * GET *
+     * Render the page
+     */
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../..", "public", "index.html"));
     });
