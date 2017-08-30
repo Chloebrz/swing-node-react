580173ba0c82bfc9118a19411c054fab9313731a
Chloebrz <chloe.brouzes@hotmail.fr>
2017-08-30 10:04:25 +0200

    Cancel button added on add and edit pages - Error messages added for name, legend and file on add and edit page

--- app/components/admin/pictureForm.js
+++ app/components/admin/pictureForm.js
@@ -9,18 +9,31 @@ class PictureForm extends Component {
             file: null,
             name: props.name || "",
             legend: props.legend || "",
-            res: props.res || ""
+            res: props.res || "",
+            errorName: false,
+            errorLegend: false,
+            errorFile: false
         };
     }
 
     handleSubmit(e) {
         e.preventDefault();
 
-        this.props.handleSubmit({
-            name: this.state.name,
-            img: this.state.file,
-            legend: this.state.legend
-        });
+        this.setState(
+            {
+                errorName: this.state.name.trim().length < 5,
+                errorLegend: this.state.legend.trim().length < 15,
+                errorFile: !this.state.file
+            },
+            function() {
+                if (!this.state.errorName && !this.state.errorLegend && !this.state.errorFile)
+                    this.props.handleSubmit({
+                        name: this.state.name,
+                        img: this.state.file,
+                        legend: this.state.legend
+                    });
+            }
+        );
     }
 
     handleImageChange(e) {
@@ -35,7 +48,8 @@ class PictureForm extends Component {
                     data: upload.target.result,
                     filename: file.name,
                     filetype: file.type
-                }
+                },
+                errorFile: false
             });
         };
         reader.readAsDataURL(file);
@@ -43,12 +57,27 @@ class PictureForm extends Component {
 
     handleNameChange(e) {
         e.preventDefault();
-        this.setState({ name: e.target.value });
+        this.setState({ name: e.target.value, errorName: false });
     }
 
     handleLegendChange(e) {
         e.preventDefault();
-        this.setState({ legend: e.target.value });
+        this.setState({ legend: e.target.value, errorLegend: false });
+    }
+
+    renderErrorName() {
+        if (!this.state.errorName) return;
+        return <div className="error">5 caractères minimum</div>;
+    }
+
+    renderErrorLegend() {
+        if (!this.state.errorLegend) return;
+        return <div className="error">15 caractères minimum</div>;
+    }
+
+    renderErrorImg() {
+        if (!this.state.errorFile) return;
+        return <div className="error">Sélectionner une image</div>;
     }
 
     render() {
@@ -60,6 +89,7 @@ class PictureForm extends Component {
                     <div className="row">
                         <div className="col-md-7">
                             <input type="file" onChange={this.handleImageChange.bind(this)} />
+                            {this.renderErrorImg()}
                             <br />
                             <br />
                             <img src={this.state.file ? this.state.file.data : this.state.res} />
@@ -73,6 +103,7 @@ class PictureForm extends Component {
                                 value={this.state.name}
                                 onChange={this.handleNameChange.bind(this)}
                             />
+                            {this.renderErrorName()}
 
                             <br />
                             <br />
@@ -85,6 +116,7 @@ class PictureForm extends Component {
                                 value={this.state.legend}
                                 onChange={this.handleLegendChange.bind(this)}
                             />
+                            {this.renderErrorLegend()}
                         </div>
                     </div>
                     <br />
@@ -96,6 +128,13 @@ class PictureForm extends Component {
                             onClick={this.handleSubmit.bind(this)}
                         >
                             Sauvegarder
+                        </button>
+                        <button
+                            className="btn btn-secondary right"
+                            type="submit"
+                            onClick={() => this.props.history.push("/admin")}
+                        >
+                            Annuler
                         </button>
                     </div>
                 </form>
